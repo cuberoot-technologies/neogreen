1246f8f01be4b594200e484cd511245ead40bb02
Abhilash Srivastava <abhilash@cuberoot.tech>
2025-03-12 13:21:19 +0530

    initial commit

---
+++ src/js/neogreen.js
@@ -0,0 +1,14 @@
+// For good bundling
+
+// Animate
+import "../css/animate.css"
+// Magnicic Popup
+import "../css/magnific.popup.css"
+// MeanMenu
+import "../css/meanmenu.css"
+// SwiperBundle
+import "../css/swiper.bundle.css"
+// Nice Select
+import "../css/nice-select.css"
+// Main
+import "../css/main.css"
